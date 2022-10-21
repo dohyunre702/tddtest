51f0da02d221888cf894ea9069dd573aba7481c1
dohyu <dohyunre702@gmail.com>
2022-10-21 10:54:10 +0900

    프로그래머스_괄호풀기 02 stack 활용

--- src/main/java/likeLion/SolveBracket.java
+++ src/main/java/likeLion/SolveBracket.java
@@ -1,14 +1,18 @@
 package likeLion;
 
+import java.util.Stack;
+
 public class SolveBracket {
         public boolean solution(String s) {
-            System.out.println(s.length());
-            String s1 = s.replaceAll("()","");
-            System.out.println(s);
-            System.out.println(s1);
-            System.out.println(s.length());
-
-            return false;
-
+            Stack<Character> st = new Stack<>();
+            for (int i = 0; i <s.length(); i++) {
+                if('(' == s.charAt(i)) {
+                    st.push(s.charAt(i));
+                } else if ('(' == s.charAt(i)) {
+                    if (st.empty()) return false;
+                    st.pop();
+                }
+            }
+            return st.empty();
         }
 }
