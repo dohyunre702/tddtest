3e9fba017132d7199fb90afa08768b05c56dfaeb
dohyu <dohyunre702@gmail.com>
2022-10-21 10:45:50 +0900

    프로그래머스_괄호풀기 01

---
+++ src/main/java/likeLion/SolveBracket.java
@@ -0,0 +1,14 @@
+package likeLion;
+
+public class SolveBracket {
+        public boolean solution(String s) {
+            System.out.println(s.length());
+            String s1 = s.replaceAll("()","");
+            System.out.println(s);
+            System.out.println(s1);
+            System.out.println(s.length());
+
+            return false;
+
+        }
+}
---
+++ src/test/java/likeLion/SolveBracketTest.java
@@ -0,0 +1,20 @@
+package likeLion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolveBracketTest {
+    @Test
+    @DisplayName("괄호가 잘 풀리는지")
+    void bracket() {
+        SolveBracket sb = new SolveBracket();
+        assertTrue(sb.solution("()()"));
+        assertFalse(sb.solution("(())()"));
+        assertFalse(sb.solution(")()("));
+        assertTrue(sb.solution("(()("));
+    }
+
+}
